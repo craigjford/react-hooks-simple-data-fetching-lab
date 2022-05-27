731eb8e1d9430a9493309f56e0e4e8cb70f57348
Craig Ford <craig_ford@icloud.com>
2022-05-27 18:07:25 -0400

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+    console.log('in program App')
+
+    const [isImage, setIsImage] = useState(null);
+
+    useEffect(() => { 
+        console.log('using useEffect fetching')
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(resp => resp.json())
+        .then(data => setIsImage(data.message))
+    },[])
+
+    if (!isImage) return <p>Loading....</p> 
+
+    return <img src={isImage} alt="A Random Dog" height="450px" width="450px" />
+
+
+}
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+/*
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(
+            (data) => {
+                setIsLoaded(true)
+                setImage=(data.message)
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [])   
+*/
